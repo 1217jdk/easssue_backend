69d39f0bd489fb0fb277f67f313fc800ccf51b2e
zaq1290 <zaq1290@naver.com>
2023-02-10 15:31:59 +0900

    feat: modify article log doc entity - add title, link

--- src/main/java/com/limemul/easssue/entity/ArticleLogDoc.java
+++ src/main/java/com/limemul/easssue/entity/ArticleLogDoc.java
@@ -8,7 +8,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 
 @Document(collection = "articleLog")
 @Getter
@@ -26,14 +25,20 @@ public class ArticleLogDoc {
 
     private String category;
 
-    private ArticleLogDoc(Long userId, Long articleId, String category) {
+    private String title;
+
+    private String link;
+
+    private ArticleLogDoc(User user,ArticleDoc articleDoc) {
         this.clickTime=LocalDateTime.now();
-        this.userId = userId;
-        this.articleId = articleId;
-        this.category = category;
+        this.userId = user.getId();
+        this.articleId = articleDoc.getArticleId();
+        this.category = articleDoc.getCategory();
+        this.title = articleDoc.getTitle();
+        this.link=articleDoc.getLink();
     }
 
-    public static ArticleLogDoc of(Long userId, Long articleId, String category){
-        return new ArticleLogDoc(userId,articleId,category);
+    public static ArticleLogDoc of(User user,ArticleDoc articleDoc){
+        return new ArticleLogDoc(user,articleDoc);
     }
 }
