f4d35a58fc79c2a0e49e5d0d0264a8762ae3fb96
zaq1290 <zaq1290@naver.com>
2023-02-12 16:47:34 +0900

    feat: add dash api - GET /dash/info/v2

--- src/main/resources/dashboard_wordcloud.py
+++ src/main/resources/dashboard_wordcloud.py
@@ -7,28 +7,37 @@ from wordcloud import WordCloud
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import pickle
+from pymongo import MongoClient
+
 
 def color_func(word, font_size, position,orientation,random_state=None, **kwargs):
     return("hsl({:d},{:d}%, {:d}%)".format(np.random.randint(180,220),np.random.randint(70,90),np.random.randint(50,70)))
 
+
+with open('/Users/SSAFY/Data/data/mongodb_user_info.pickle', 'rb') as f:
+    user_info = pickle.load(f)
+
+client = MongoClient(host='www.easssue.com', port=27017, username=user_info['username'], password=user_info['password'])
+db = client.get_database('easssue_data')
+coll_al = db.get_collection('articleLog')
+coll_a = db.get_collection('article')
+
+with open(f'/Users/SSAFY/Data/data/kwd/20221117_01_kwd_name_id.pickle', 'rb') as f:
+    kwds_name_id = pickle.load(f)
+
+
 def get_user_word(user_id, cursor):
-    today = datetime.datetime.now() + datetime.timedelta(hours=9)
+    today = datetime.datetime.today() + datetime.timedelta(hours=9)
     week_ago = today - datetime.timedelta(days=7)
-    today = today.date().strftime('%Y-%m-%d')
-    week_ago = week_ago.date().strftime('%Y-%m-%d')
 
-    query = f"""
-    select k.kwd_name, ak.kwd_count
-    from kwd k
-    join article_kwd ak on k.kwd_id=ak.kwd_id
-    join article a on ak.article_id=a.article_id
-    join article_log al on a.article_id=al.article_id
-    where al.user_id={user_id} and al.click_time between '{week_ago}' and '{today}';
-    """
-
-    cursor.execute(query)
-
-    result = cursor.fetchall()
+    result = []
+    articles = [item["articleId"] for item in
+                coll_al.find({"userId": user_id, "clickTime": {"$gte": week_ago, "$lte": today}})]
+    for article in articles:
+        for item in coll_a.find({"articleId": article}):
+            for kwd in item['kwds']:
+                result.append((kwds_name_id[kwd['kwd']], kwd['kwdCount']))
 
     return result
 
